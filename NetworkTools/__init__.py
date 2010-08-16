a212f62be3b602fbb41a93cd4da9dde1e92a3554
Philip Horger <campadrenalin@gmail.com>
2010-08-15 23:38:49 -0700

    Rough example version of NetworkTools utility classes

--- NetworkTools/__init__.py
+++ NetworkTools/__init__.py
@@ -1,2 +1,46 @@
-class WaveConnection():
-	def connect
+# base class that connection classes will subclass
+
+# This is basically just a suggestion to get the network
+# guys started. I really don't want to get involved in
+# that part of the program any more than I have to.
+# People who understand the wave model better than I do
+# are the people who should be building this and its
+# subclasses. - Philip
+
+class Document:
+	def __init__(self, meta):
+		self.meta = meta
+		self.structure = []
+
+	def addBlip(self, blip, position):
+		self.structure.insert(position,blip)
+
+class Blip:
+	def __init__(self, contents, meta):
+		self.contents = contents
+		self.meta = meta
+
+class Plugin:
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+
+	def connect(self):
+		print "Connected"
+
+	def disconnect(self):
+		print "Disconnected"
+
+	def query(self, query, callback):
+		print "Searching for waves that match '"+query+"'"
+		wavelet_list = []
+		callback(wavelet_list)
+
+	def getStructure(self, wavelet_id, callback):
+		print "Retrieving the structure of wavelet",wavelet_id
+		wavelet_tree = {}
+		callback(wavelet_tree)
+
+	def getBlip(self, blip_id):
+		print "Retrieving blip contents of",blip_id
+		return Blip()
