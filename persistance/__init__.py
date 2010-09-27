b96dc4a882260df70f01cf6286544fb7021fc3ed
Philip Horger <campadrenalin@gmail.com>
2010-09-27 12:05:59 -0700

    Default subfolders added to .pytide

--- persistance/__init__.py
+++ persistance/__init__.py
@@ -1,8 +1,15 @@
-from os import mkdir
+from os import makedirs
 import os.path
+
+default_subfolders = ['cache', 'cache/outbound',
+		'contacts',
+		'attachments']
 
 def init_dir(path="~/.pytide/"):
 	expanded = os.path.expanduser(path)
 	if not os.path.isdir(expanded):
 		os.makedirs(expanded)
+	for folder in default_subfolders:
+		if not os.path.isdir(os.path.join(expanded,folder)):
+			os.makedirs(os.path.join(expanded,folder))
 	return expanded
