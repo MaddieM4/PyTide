bfde0366db118fa90f847f07168753ca870cb70c
Philip Horger <campadrenalin@gmail.com>
2010-09-28 12:43:59 -0700

    Toolbar JS moved to appropriate file

--- gui/html/jslib/toolbar.js
+++ gui/html/jslib/toolbar.js
@@ -8,4 +8,65 @@ searchbox.focusout(function(){
 	$('.search').removeClass("highlighted")
 });
 
+hiddenTools = new Array();
+
+function hideTool(jqobj) {
+	hiddenTools[hiddenTools.length] = {"class":jqobj.attr("class"),"label":jqobj.text()};
+	jqobj.addClass("invisible");
+}
+
+function clearTools() {hiddenTools = new Array()}
+
+function pullTools() {
+	retstring = ""
+	for (i in hiddenTools) {
+		retstring+="<li class='"+hiddenTools[i]['class']+"'><div>"+hiddenTools[i]['label']+"</div>";
+	}
+	return retstring;
+}
+
+adjusting=false;
+// adjust toolbar in resize events
+function adj_toolbar(){
+	if (adjusting) return false;
+	adjusting=true;
+	clearTools();
+	toolbar = $('.toolbar ul:not(.invisible)');
+	totalwidth = $(window).width();
+	runningwidth = 0;
+	showMore = false;
+	if (options['tbshorten']=='autoshorten') {
+		// test to see if normal style is too long
+		$('.toolbar').removeClass('noicons').removeClass('nolabels');
+		tc = toolbar.children(':not(.more)');
+		last = toolbar.children(':not(.more):last');
+		tc.removeClass('invisible').removeClass('icononly').each(function(index){
+			t = $(tc[tc.length-index-1]);
+			var wid = last.position().left+last.outerWidth();
+			if (wid > totalwidth || last.position().top > 58) {
+				t.addClass('icononly');
+			}
+		});
+		runningwidth=0;
+	}
+	toolbar.children(':not(.more)').removeClass('invisible').each(function(){
+		t = $(this);
+		runningwidth+=t.outerWidth();
+		if (runningwidth-4 > totalwidth || t.position().top > 58) {
+			hideTool(t);
+			showMore = true;
+		}
+	});
+	if (showMore) {
+		$('.toolbar .more').removeClass('invisible')
+	} else {
+		if (!$('.toolbar .more').hasClass('active')) {
+			$('.toolbar .more').addClass('invisible');
+		}
+	}
+	adjusting=false;
+}
+
+$(window).resize(adj_toolbar);
+adj_toolbar()
 
