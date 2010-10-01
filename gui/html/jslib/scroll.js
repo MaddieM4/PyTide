90c39078f7f2ccb09805f6972b99e15217a015a4
Philip Horger <campadrenalin@gmail.com>
2010-09-30 18:26:06 -0700

    Modified scrollbar so releasing anywhere cancels drag

--- gui/html/jslib/scroll.js
+++ gui/html/jslib/scroll.js
@@ -2,31 +2,38 @@ function bindScrollbar(scrollbar, scrollable) {
 	$(scrollbar).data('target',scrollable);
 }
 
-scrollTarget = null;
-scrollDriver = null;
-
-$('.scrollregion').mousedown(function (){
-	// start a drag
-	scrollDriver = $($(this).children('.scrollbar')[0]);
-	scrollTarget = scrollDriver.data('target');
-	//console.log(scrollDriver)
-	//console.log(scrollTarget)
-}).mouseup(function() {
-	// end it
-	scrollDriver = null;
-	scrollTarget = null;
-}).mousemove(function(e){
+function scroll(e) {
 	// this is where the magic happens
 	if (scrollDriver != null) {
 		// find the percentage at the mouse position
-		percent = Math.max(Math.min((e.pageY-124)/$(this).height()*100,100),0)
+		percent = Math.max(Math.min((e.pageY-124)/scrollDriver.parent().height()*100,100),0)
 		scrollDriver.css('top',""+percent+"%");
 		if (scrollTarget != null) {
 			scrollTarget = $(scrollTarget)
 			lastkid = $(scrollTarget).children().last()
 			maxscroll = lastkid.position().top+lastkid.outerHeight()+scrollTarget.scrollTop()-scrollTarget.height();
-			//console.log(maxscroll)
 			scrollTarget.scrollTop(maxscroll*percent/100)
 		}
 	}
+}
+
+scrollTarget = null;
+scrollDriver = null;
+
+$('.scrollregion').mousedown(function (e){
+	// start a drag
+	scrollDriver = $($(this).children('.scrollbar')[0]);
+	scrollTarget = scrollDriver.data('target');
+	scroll(e)
+}).mouseup(function() {
+	// end it
+	scrollDriver = null;
+	scrollTarget = null;
+}).mousemove(function(e){
+	scroll(e)
 });
+
+$('body').mouseup(function() {
+	scrollDriver = null;
+	scrollTarget = null;
+})
