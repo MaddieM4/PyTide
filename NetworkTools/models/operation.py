cc7fdd62aa2b2fffef67ff1eade5596db5e2ccbb
Nathanael Abbotts <nat.abbotts@gmail.com>
2010-10-11 11:23:19 +0100

    Removed the models.py file, seperating the code out into a models directory, with (currently) 4 files in it.
    Added code to model a Wave (collection of wavelets), and a partial Wavelet model.
    
    Fixed all "classname.__init__(self, args)" calls, making them the more pythonic "super(self.__class__, self).__init__(args)"
    
    TODO:
    	* Go through all code in the NetworkTools directory, and fix the (now broken) "import models" imports.
    	* Go through all other code, and make sure that models are not directly imported, and if they are, to write an interface to them instead.
    	* Complete the OT module (currently named "operations.py")

---
+++ NetworkTools/models/operation.py
@@ -0,0 +1,48 @@
+#
+# Copyright Notice:
+#
+# Copyright 2010    Nathanael Abbotts (nat.abbotts@gmail.com),
+#                   Philip Horger,
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+
+import datetime
+from collections import deque
+
+class Operation:
+	def __init__(self, timestamp=datetime.datetime.now()):
+		pass
+
+	def apply(document):
+		pass
+	    
+class Operation_Queue(deque):
+    """A queue of Operation objects
+
+    When instantiating, you can provide any number of Operation instances as
+    operations."""
+    def __init__(self, *args, position = 0,): #(self, wavelet, *args, position)
+        """Creates an Operation Queue.
+        
+        *args should all be Operation objects, which will be added to the queue
+            in sequential order.
+        """
+        super(super.__class__, self).__init__(args)
+        self.position = position
+        # Note that this operation queue does not have a wavelet attribute - 
+        # that is because I believe that a wavelet should have an opqueue, 
+        # not the reverse. This will be discussed elsewhere though.
+    def append(self, position, operation, arg):
+        op = Operation(position, operation, arg)
+        super(self.__class__, self).append(op)
