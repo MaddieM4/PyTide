f7bacef5256260dd201051a244cc04fe614562fe
Nathanael Abbotts <nat.abbotts@gmail.com>
2010-11-28 08:51:11 +0000

    Started a document model and a new ot script. They will replace existing ones once they are completed.

---
+++ NetworkTools/models/ot.py
@@ -0,0 +1,59 @@
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from multiprocessing import Process, Queue, Pipe
+#import threading
+from gtk.gdk import threads_enter, threads_leave
+from collections import deque
+
+class ServerBuffer(Process):
+    def __init__(self):
+        super(ServerBuffer, self).__init__()
+        self._private_pipe, self._public_pipe = Pipe()
+    @property
+    def pipe(self):
+        return self._public_pipe
+
+    @property
+    def server_connection(self):
+        if hasattr(self, '_serverconnection'):
+            return self._serverconnection
+        else:
+            return None
+    @server_connection.setter
+    def server_connection(self, connection):
+        self._serverconnection = connection
+    # ------------------- OVERIDE THESE -------------------
+    def something_to_be_overridden(self):
+        pass
+    
+class Composer(Process):
+    def __init__(self):
+        super(Composer, self).__init__()
+
+class Transformer(Process):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        
+
+# ---------------------------- DESIGN INTENTIONS ------------------------------
+#
+# This module will contain the following:
+#   * A 'server buffer', which will control the sending and recieving of
+#       operations to and from the server.
+#       The buffer will have it's own processes (2), so as the sending and
+#       recieving  of operations will not make the rest of the app 'hang'.
+#       The buffer will send and recieve operations to/from the transformer by
+#       means of a Pipe.
+#   * A 'composer' to combine operations.
+#   * A 'transformer' to transform operations.
+#   * A Manager class of some sort, to organise the whole shebang.
