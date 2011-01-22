52c41d6124c3040764c4e0021cebdcb5c89ceccc
Nathanael Abbotts <nat.abbotts@gmail.com>
2011-01-22 12:48:17 +0000

    In compose.py, added 3rd rule/guideline to dev guide, and ensured that current code complies with guide.
    No new code as such, just code documentation.

--- NetworkTools/models/compose.py
+++ NetworkTools/models/compose.py
@@ -16,20 +16,33 @@ import operation
 class CompositionException(Exception):
     """A basic composition error."""
 
-
-# Developer's guide to contributing to this composition code
-#
+# --------------------------------------------------------------------------- #
+#                                                                             #
+#          Developer's guide to contributing to this composition code         #
+#                                                                             #
+# --------------------------------------------------------------------------- #
 # As there are so many different compositions (no of instruction types ^2),
 # it is important that all of the code in the compose function is implemented
 # in a similar fashion. The following guidelines (nay, rules) should make it
 # simple for anyone to contribute.
 #
 # 1) Each of the different compositions should have a documentation string.
-#      The documentation string should contain an exact description of what
-#      will is done to compose the two instructions.
+#
+#    The docstring should begin with either "Composeable." or "Uncomposeable."
+#
+#    If the instructions are composeable, then the docstring should contain an
+#    exact description of what will be done to compose the two instructions.
 #
 # 2) Anything that is not blindingly obvious needs a code comment.
-#      If it isn't as simple as a = 1; b = 2; x = a + b; then document it.
+#
+#    If it isn't as simple as a = 1; b = 2; x = a + b; then document it.
+#
+# 3) Variables a & b should not be modified, but instead treated as constants.
+#
+#    This is simple enough - don't change any of a or b's attributes, and if
+#    a & b are composeable, return a *new* instruction, not a modified a or b.
+#
+
 def compose(a, b):
     """Composes two *adjacent* instructions into a single instruction.
 
@@ -52,7 +65,7 @@ def compose(a, b):
     if a.document is not b.document:
         raise CompositionException(("To compose, instructions must pertain to "
                                     "the same document."))
-    
+
     document = a.document
     if a.name is 'AnnotationBoundary':
         if b.name is 'AnnotationBoundary':
@@ -140,6 +153,10 @@ def compose(a, b):
         elif b.name is 'DeleteOpenElement':
             pass
         elif b.name is 'InsertCharacters':
+            """Composeable.
+            The composed instruction simply joins the two strings of inserted
+            characters, a first, then b.
+            """
             string = a.string + b.string
             return instruction.InsertCharacters(document=document,
                                                 string=string)
@@ -205,6 +222,10 @@ def compose(a, b):
         elif b.name is 'InsertOpenElement':
             pass
         elif b.name is 'Retain':
+            """Composeable.
+            The composed instruction is another Retain, with the 'count' set
+            as the sum of the counts of a & b.
+            """
             count = a.count + b.count
             return instruction.Retain(document=document,
                                       count=count)
