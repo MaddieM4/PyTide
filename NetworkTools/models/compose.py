d6fea6f6b9a00d88be0f7ee7a6fbbf17aeff6e44
Nathanael Abbotts <nat.abbotts@gmail.com>
2011-01-22 12:06:18 +0000

    Added Dev's guide to contributing to the composition function.

--- NetworkTools/models/compose.py
+++ NetworkTools/models/compose.py
@@ -13,6 +13,23 @@
 import instruction
 import operation
 
+class CompositionException(Exception):
+    """A basic composition error."""
+
+
+# Developer's guide to contributing to this composition code
+#
+# As there are so many different compositions (no of instruction types ^2),
+# it is important that all of the code in the compose function is implemented
+# in a similar fashion. The following guidelines (nay, rules) should make it
+# simple for anyone to contribute.
+#
+# 1) Each of the different compositions should have a documentation string.
+#      The documentation string should contain an exact description of what
+#      will is done to compose the two instructions.
+#
+# 2) Anything that is not blindingly obvious needs a code comment.
+#      If it isn't as simple as a = 1; b = 2; x = a + b; then document it.
 def compose(a, b):
     """Composes two *adjacent* instructions into a single instruction.
 
@@ -32,6 +49,11 @@ def compose(a, b):
         # code to handle what happens when they do.
 
     """
+    if a.document is not b.document:
+        raise CompositionException(("To compose, instructions must pertain to "
+                                    "the same document."))
+    
+    document = a.document
     if a.name is 'AnnotationBoundary':
         if b.name is 'AnnotationBoundary':
             pass
@@ -118,7 +140,6 @@ def compose(a, b):
         elif b.name is 'DeleteOpenElement':
             pass
         elif b.name is 'InsertCharacters':
-            document = a.document
             string = a.string + b.string
             return instruction.InsertCharacters(document=document,
                                                 string=string)
@@ -184,7 +205,6 @@ def compose(a, b):
         elif b.name is 'InsertOpenElement':
             pass
         elif b.name is 'Retain':
-            document = a.document
             count = a.count + b.count
             return instruction.Retain(document=document,
                                       count=count)
